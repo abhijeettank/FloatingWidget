dbe7144e3ee4b726b267048535300742b5a327af
Abhi <abhijeet.tank@gmail.com>
2017-08-11 10:17:36 +0530

    added comment

--- app/src/main/java/com/creativeinfoway/floatingwidget/MainActivity.java
+++ app/src/main/java/com/creativeinfoway/floatingwidget/MainActivity.java
@@ -14,7 +14,7 @@ import com.creativeinfoway.floatingwidget.Service.FloatingViewService;
 public class MainActivity extends AppCompatActivity {
 
     private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION=2084;
-
+//comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
